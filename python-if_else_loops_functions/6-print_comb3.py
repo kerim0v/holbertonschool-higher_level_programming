746ab487991bedea7d6916577d1685981905a119
kerim0v <kerimvtamerlan@gmail.com>
2026-04-23 12:43:24 +0000

    a

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        if i != 8 and j != 9:
+        if not (i == 8 and j == 9):
             print("{}{}, ".format(i, j), end='')
 print("{}".format(89))
