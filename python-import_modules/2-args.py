37c2cf5fad3452e9395906b1d529af4e59b85ac5
kerim0v <kerimvtamerlan@gmail.com>
2026-04-27 10:18:08 +0000

    a

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    num = len(argv) - 1
+    if num == 1:
+       print("1 argument:") 
+    elif num == 0:
+        print("0 arguments.") 
+    else:
+        print ("{} arguments:".format(num))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
