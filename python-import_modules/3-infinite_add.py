f4fcad173c5a344b3ae51498e2df7613c1425348
kerim0v <kerimvtamerlan@gmail.com>
2026-04-27 10:26:41 +0000

    a

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,7 @@
 from sys import argv
 
 if __name__ == "__main__":
-    for i in range (0, len(argv)):
-        sum = sum + argv[i]
+    sum = 0
+    for i in range(1, len(argv)):
+        sum = sum + int(argv[i])
     print(sum)
