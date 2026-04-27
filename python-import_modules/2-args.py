eee154729088ad3cd108c80677420e7fc327fa11
kerim0v <kerimvtamerlan@gmail.com>
2026-04-27 10:21:22 +0000

    a

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,11 @@ from sys import argv
 if __name__ == "__main__":
     num = len(argv) - 1
     if num == 1:
-       print("1 argument:") 
+        print("1 argument:")
     elif num == 0:
-        print("0 arguments.") 
+        print("0 arguments.")
     else:
-        print ("{} arguments:".format(num))
+        print("{} arguments:".format(num))
 
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
