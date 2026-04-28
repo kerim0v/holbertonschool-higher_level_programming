59de18bf65d2ea8f5b6ad27d7531a2e3c305457c
kerim0v <kerimvtamerlan@gmail.com>
2026-04-28 14:59:09 +0000

    a

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        tuple_a = (0, None)
+        return tuple_a
+    return tuple_b = (len(sentence), sentence[0])
+
