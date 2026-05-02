a611b2240fcbc1ca97eb833ca64dc4f854d27fa4
kerim0v <kerimvtamerlan@gmail.com>
2026-05-02 18:58:16 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
         return None
 
     values = {
-        'I': 1, 'V': 5, 'X' = 10,
+        'I': 1, 'V': 5, 'X': 10,
         'L': 100, 'D': 500, 'M': 1000
     }
 
