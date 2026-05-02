18c93966e5ea710d29f763b4a055b8db2e40c2cb
kerim0v <kerimvtamerlan@gmail.com>
2026-05-02 18:57:52 +0000

    a

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return None
+
+    values = {
+        'I': 1, 'V': 5, 'X' = 10,
+        'L': 100, 'D': 500, 'M': 1000
+    }
+
+    result = 0
+    for i in range (len(roman_string)):
+        current = values.get(roman_string[i], 0)
+        next_val = values.get(roman_string[i + 1], 0) if i + 1 < len(roman_string) else 0
+
+        if current < next_val:
+            result -= current
+        else:
+            result += current
+
+    return result
