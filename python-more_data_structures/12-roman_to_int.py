55838517b72b09545a9600256344698ef93392fb
kerim0v <kerimvtamerlan@gmail.com>
2026-05-02 19:03:49 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -13,8 +13,9 @@ def roman_to_int(roman_string):
     result = 0
     for i in range(len(roman_string)):
         current = values.get(roman_string[i], 0)
-        next_val = values.get(roman_string[i + 1], 0)
-        if i + 1 < len(roman_string) else 0
+        next_val = 0
+        if i + 1 < len(roman_string):
+            next_val = values.get(roman_string[i + 1], 0)
 
         if current < next_val:
             result -= current
