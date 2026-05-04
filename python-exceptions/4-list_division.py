7b92384508b150415b7a148bb8edca6ce78c0b50
kerim0v <kerimvtamerlan@gmail.com>
2026-05-04 22:09:33 +0000

    a

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
-            print("divison by 0")
+            print("division by 0")
         except IndexError:
             print("out of range")
         finally:
