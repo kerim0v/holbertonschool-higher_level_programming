ece933839d26e02a09fe3e6c86962ba54872f063
kerim0v <kerimvtamerlan@gmail.com>
2026-05-04 21:40:09 +0000

    a

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for item in range(0, x):
+        try:
+            print(my_list[item], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
