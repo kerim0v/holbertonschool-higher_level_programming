a3103988a5514fedca4ceffe283ec54d357d16b5
kerim0v <kerimvtamerlan@gmail.com>
2026-05-11 21:58:46 +0000

    a

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,18 +15,18 @@ def text_indentation(text):
     Raises:
         TypeError: If text is not a string.
     """
-if not isinstance(text, str):
-        raise TypeError("text must be a string")
-    i = 0
-    while i < len(text) and text[i] == " ":
-        i += 1
-    while i < len(text):
-        if text[i] in ".?:":
-            print(text[i])
-            print()
+    if not isinstance(text, str):
+            raise TypeError("text must be a string")
+        i = 0
+        while i < len(text) and text[i] == " ":
             i += 1
-            while i < len(text) and text[i] == " ":
+        while i < len(text):
+            if text[i] in ".?:":
+                print(text[i])
+                print()
                 i += 1
-        else:
-            print(text[i], end="")
-            i += 1
+                while i < len(text) and text[i] == " ":
+                    i += 1
+            else:
+                print(text[i], end="")
+                i += 1
