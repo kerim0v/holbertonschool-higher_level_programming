2fd7461bb59abb7a8e1d05f050fa9983de6377f6
kerim0v <kerimvtamerlan@gmail.com>
2026-05-18 07:20:09 +0000

    a

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """ initialize a new square"""
+
+        if not is isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise   ValueError("size must be >= 0")
+        self.__size = size
