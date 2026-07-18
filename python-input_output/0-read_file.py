f734e7648a12838cfa9747d83448350ca65f043e
kerim0v <kerimvtamerlan@gmail.com>
2026-07-18 14:42:52 +0000

    a

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""Module that reads and prints the content of a UTF8 text file"""
+
+
 def read_file(filename=""):
+    """Reads and prints"""
+    
     with open(filename, r) as f:
         content = f.read()
         print(content)
