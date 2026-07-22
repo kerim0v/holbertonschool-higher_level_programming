5a9524797087a3a650083a1ae8de88fed3c3a5b1
kerim0v <kerimvtamerlan@gmail.com>
2026-07-22 16:31:50 +0000

    a

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module writes and returns"""
+
+
+class Student:
+    """asdadsadd"""
+
+    def __init__(self, first_name, last_name, age):
+        """init chars"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """asdads"""
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+        return self.__dict__
