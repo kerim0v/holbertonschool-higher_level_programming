530ff319a66a386c57380a4247afac4a941bd8c8
kerim0v <kerimvtamerlan@gmail.com>
2026-07-26 02:03:08 +0400

    Make script executable

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Prints the State object with the given name from hbtn_0e_6_usa."""
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(
+        State.name == state_name
+    ).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print(state.id)
+
+    session.close()
