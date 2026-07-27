e7ef30fd723e55b205bc8ac3c88362c61a840c8e
kerim0v <kerimvtamerlan@gmail.com>
2026-07-28 03:38:58 +0400

    Add multi_c script

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const count = parseInt(args[0]);
+
+if (isNaN(count)) {
+  console.log('Missing number of occurrences');
+} else {
+  let output = '';
+  for (let i = 0; i < count; i++) {
+    output += 'C is fun\n';
+  }
+  console.log(output.trim());
+}
