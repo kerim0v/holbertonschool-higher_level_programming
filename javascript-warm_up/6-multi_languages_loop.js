73d5f0ccb62038a240b2b4a77427b8a80408b95f
kerim0v <kerimvtamerlan@gmail.com>
2026-07-28 03:38:11 +0400

    Add multi-language loop script

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output = '';
+
+for (let i = 0; i < languages.length; i++) {
+  output += languages[i] + '\n';
+}
+
+console.log(output.trim());
