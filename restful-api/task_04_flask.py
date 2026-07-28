72f8e2c183f7abf5609f17b7ddeac8a1543522e2
kerim0v <kerimvtamerlan@gmail.com>
2026-07-28 15:33:16 +0400

    a

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -10,7 +10,7 @@ users = {}
 @app.route("/")
 def home():
     """Root endpoint — simple welcome message."""
-    return "Welcome to thpe Flask API!"
+    return "Welcome to the Flask API!"
  
  
 @app.route("/data")
