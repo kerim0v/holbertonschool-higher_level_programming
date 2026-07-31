3d66e2c6d843ab2b9e04f8522c3d06af1d220242
kerim0v <kerimvtamerlan@gmail.com>
2026-07-31 19:18:22 +0400

    a

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
@@ -16,11 +18,13 @@ def about():
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/items')
 def items():
     with open('items.json') as f:
         data = json.load(f)
     return render_template('items.html', items=data['items'])
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
